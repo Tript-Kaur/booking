6605febb5f170b2f0afba797288631f64fc323f9
Tript Kaur <triptkaur@gmail.com>
2023-04-15 06:02:15 +0530

    yamal commit

--- src/main/java/in/aws/practice/SpringBootApigateWay/SpringBootApigateWayApplication.java
+++ src/main/java/in/aws/practice/SpringBootApigateWay/SpringBootApigateWayApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootApigateWayApplication {
-	public static void main(String[] args) {
+public class SpringBootApigateWayApplication { public static void main(String[] args) {
 		SpringApplication.run(SpringBootApigateWayApplication.class, args);
 	}
 
